9e06e18dfa0aecded93e6320cb70cc827babba4e
Alin Straps <strapples.ethermail@ud.me>
2025-01-07 05:51:13 -0600

    cleaning up a few duplicate files

--- preload 2.js
+++
@@ -1,7 +0,0 @@
-const { contextBridge, ipcRenderer } = require("electron");
-
-// Expose ipcRenderer methods to the renderer process
-contextBridge.exposeInMainWorld("electronAPI", {
-  invoke: (channel, data) => ipcRenderer.invoke(channel, data), // Async calls
-  send: (channel, data) => ipcRenderer.send(channel, data),     // Fire-and-forget calls
-});
